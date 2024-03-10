78095c4397d56d74e6648b77ace2eadb315d7890
wkdwndus0409 <jjy990409@naver.com>
2024-03-10 15:41:53 +0900

    Profile 도메인 수정 - 팔로우/팔로잉 로직 추가 - 공개범위 로직 추가 - 좋아요 로직 보류 (어떤 도메인에 위치시키는 게 좋을지)

--- src/main/java/com/show/Profile.java
+++ src/main/java/com/show/Profile.java
@@ -2,34 +2,79 @@ package com.show;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Profile {
+    final String profileId;
     String picture;
-    final String nickname;
-    final int profileId;
+    String nickname;
     String introduction;
-    List<String> videos;
     List<Profile> followings;
     List<Profile> followers;
-    List<String> likes = new ArrayList<>();
+    List<String> uploadedVideos;
+    List<String> likedVideos;
+    String visibility;
 
-    public Profile(String nickname, int profileId) {
-        this.nickname = nickname;
+    public Profile(String profileId, String nickname) {
         this.profileId = profileId;
+        this.nickname = nickname;
+
+        this.followings = new ArrayList<>();
+        this.followers = new ArrayList<>();
+        this.visibility = "public";
     }
 
-    private boolean isAlreadyLiked(String video) {
-        return likes.contains(video);
+    public void changePicture(String newPicture) {
+        this.picture = newPicture;
     }
 
-    public void like(String video) {
-        // 1. video가 likes 안에 있는지 찾는다.
-        // 2. 1 조건에 만족하면 likes에서 video를 지운다.
-        // 3. 1 조건에 만족하지 않으면 likes에 video를 추가한다.
-        if(isAlreadyLiked(video)) {
-            likes.remove(video);
-            return;
+    public void changeNickname(String newNickname) {
+        this.nickname = newNickname;
+    }
+
+    public void changeIntroduction(String newIntroduction) {
+        this.introduction = newIntroduction;
+    }
+
+    private boolean isAlreadyFollowed(Profile user) {
+        return followings.contains(user);
+    }
+
+    public void follow(Profile user) {
+        if (isAlreadyFollowed(user)) {
+            followings.remove(user);
+            user.followers.remove(this);
+            return ;
         }
-        likes.add(video);
+        followings.add(user);
+        user.followers.add(this);
     }
+
+    private boolean isPublic() {
+        return Objects.equals(this.visibility, "public");
+    }
+
+    public void changeVisibility() {
+        if (isPublic()) {
+            this.visibility = "private";
+            return ;
+        }
+        this.visibility = "public";
+    }
+
+//    like 기능 보류
+//    private boolean isAlreadyLiked(String video) {
+//        return likes.contains(video);
+//    }
+//
+//    public void like(String video) {
+//        // 1. video가 likes 안에 있는지 찾는다.
+//        // 2. 1 조건에 만족하면 likes에서 video를 지운다.
+//        // 3. 1 조건에 만족하지 않으면 likes에 video를 추가한다.
+//        if(isAlreadyLiked(video)) {
+//            likes.remove(video);
+//            return;
+//        }
+//        likes.add(video);
+//    }
 }
--- src/test/java/com/show/ProfileTest.java
+++ src/test/java/com/show/ProfileTest.java
@@ -5,13 +5,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfileTest {
+
     @Test
-    void like() {
-        String video = "mp4";
-        Profile profile = new Profile("juan", 1);
-        profile.like(video);
+    void follow() {
+        Profile me = new Profile("@jjy1004", "judy");
+        Profile user = new Profile("@kmk2024", "pingu");
+        me.follow(user);
 
-
-        assertEquals(video, profile.likes.get(0));
+        assertEquals("pingu", me.followings.get(0).nickname);
+        assertEquals("judy", user.followers.get(0).nickname);
     }
+
+    @Test
+    void changeVisibility() {
+        Profile me = new Profile("@jjy1004", "judy");
+        me.changeVisibility();
+
+        assertEquals("private", me.visibility);
+    }
+//    @Test
+//    void like() {
+//        String video = "mp4";
+//        Profile profile = new Profile("jjy_0123", "juan");
+//        profile.like(video);
+//
+//
+//        assertEquals(video, profile.likes.get(0));
+//    }
 }
